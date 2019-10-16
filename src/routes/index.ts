ed3179c8391b39d088a5f41e5116357f99407162
mikan <aayush6194@gmail.com>
2019-10-15 21:59:39 -0500

    changed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import UserController from './user-controller';
 
 const SetRoutes = (app : any) => {
-  app    
-    .get('/api/test', (req: any,res: any)=>res.send('Online'))
+  app     
     .post('/api/register', UserController.register)
     .post('/api/search-user', UserController.searchUser)
-    .post('/api/update-user', UserController.searchUser); //update classification
+    .post('/api/update-user', UserController.searchUser) //update classification
+    .get('/', (req: any,res: any)=>res.send({ message: 'Online' }));
     //post to increase count
 }
 
