65030d5b46c47e53dbace7c83c2c750c708df43c
mikan <aayush6194@gmail.com>
2019-10-15 22:20:15 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { Port, MongoURI } from "./config";
 
 const app = express();
 InitMiddleWare(app);
-SetRoutes(app);
+//SetRoutes(app);
 // app.use('/', 
 //   express.static((require('path').resolve('./client/dist/wesleyApp/')))
 // )
@@ -14,17 +14,21 @@ SetRoutes(app);
 app.listen(Port, "localhost");
 console.log(`Running on Port : ${Port}`);
 
-ConnectDB(MongoURI).catch((err: Error) => {
-  console.log("Unable to Connect");
-});
+// ConnectDB(MongoURI).catch((err: Error) => {
+//   console.log("Unable to Connect");
+// });
 
 //app.get('*', (req: Request, res: Response)=> res.redirect('/'));
 
-app.use((err: any, req: Request, res: Response, next: NextFunction): void => {
+// app.use((err: any, req: Request, res: Response, next: NextFunction): void => {
 
-    res.status(400)
-        .send({ success: false, message: err.message || err });
-  }
-);
+//     res.status(400)
+//         .send({ success: false, message: err.message || err });
+//   }
+// );
+
+app.use('/', (req : any, res: any)=>{
+  res.send({ message : "online"})
+})
 
 export default app;
