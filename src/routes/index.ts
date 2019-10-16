682aab37b82049d505e586010e7a4c42d3836165
mikan <aayush6194@gmail.com>
2019-10-15 21:20:09 -0500

    api online test added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import UserController from './user-controller';
 
 const SetRoutes = (app : any) => {
   app    
+    .get('/api/test', (req: any,res: any)=>res.send('Online'))
     .post('/api/register', UserController.register)
     .post('/api/search-user', UserController.searchUser)
     .post('/api/update-user', UserController.searchUser); //update classification
